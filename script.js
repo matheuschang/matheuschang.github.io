7ad4eacf2b8f69fb41a1e9f342b4651575f0311d
Matheus Chang <61498020+matheuschang@users.noreply.github.com>
2023-03-25 00:04:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,14 @@
-// Load the current counter value from the GitHub API
-fetch("https://api.github.com/repos/{username}/my-website/contents/counter.txt")
-  .then(response => response.json())
-  .then(data => {
-    // Convert the base64-encoded content to a string
-    const content = atob(data.content);
-    // Parse the counter value from the string
-    const counter = parseInt(content);
-    // Set the counter element's text to the counter value
-    document.getElementById("counter").textContent = counter;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// Load the current counter value from local storage
+let counter = localStorage.getItem("counter") || 0;
+// Set the counter element's text to the counter value
+document.getElementById("counter").textContent = counter;
 
 // Add an event listener to the add button
 document.getElementById("add-button").addEventListener("click", () => {
-  // Load the current counter value from the GitHub API
-  fetch("https://api.github.com/repos/{username}/my-website/contents/counter.txt")
-    .then(response => response.json())
-    .then(data => {
-      // Convert the base64-encoded content to a string
-      const content = atob(data.content);
-      // Parse the counter value from the string
-      let counter = parseInt(content);
-      // Increment the counter value
-      counter++;
-      // Update the counter element's text to the new counter value
-      document.getElementById("counter").textContent = counter;
-      // Encode the new counter value as base64
-      const encodedContent = btoa(counter.toString());
-      // Update the counter.txt file on GitHub using the GitHub API
-      fetch("https://api.github.com/repos/{username}/my-website/contents/counter.txt", {
-        method: "PUT",
-        headers: {
-          "Authorization": "token {access_token}",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          message: "Update counter",
-          content: encodedContent,
-          sha: data.sha
-        })
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  // Increment the counter value
+  counter++;
+  // Update the counter element's text to the new counter value
+  document.getElementById("counter").textContent = counter;
+  // Save the new counter value to local storage
+  localStorage.setItem("counter", counter);
 });
